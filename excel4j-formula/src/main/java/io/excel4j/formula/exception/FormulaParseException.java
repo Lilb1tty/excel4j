4302d184dcedc082e8a504c99fe6a674ae9cdb7a
LilBitty <2227879499@qq.com>
2026-05-01 17:52:35 +0800

    feat(formula): exception hierarchy

---
+++ excel4j-formula/src/main/java/io/excel4j/formula/exception/FormulaParseException.java
@@ -0,0 +1,9 @@
+package io.excel4j.formula.exception;
+
+import io.excel4j.core.exception.ExcelException;
+
+public class FormulaParseException extends ExcelException {
+    public FormulaParseException(String message) {
+        super(message);
+    }
+}
